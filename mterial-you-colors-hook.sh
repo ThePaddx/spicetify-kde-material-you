c445e2bc4a50abb521a16b409995a8a27becaa44
ThePaddx <paddx.k@live.de>
2025-05-07 15:24:45 +0200

    Add files via upload

---
+++ mterial-you-colors-hook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python /home/patrickk/.config/spicetify/Themes/material-you-spicetify.py
+
+
+## starts spicetify in watch mode to search and apply changes and kills the process after changing colors.
+spicetify watch -l -q &
